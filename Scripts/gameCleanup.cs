b0c6aaac885100c243b58b6dc75fde3efb42d8c9
Janis Zusevics <j.zusevics@gmail.com>
2023-11-14 21:15:03 +0000

    Added documentation

--- Scripts/gameCleanup.cs
+++ Scripts/gameCleanup.cs
@@ -1,18 +1,22 @@
+/// <summary>
+/// Destroys game objects that fall below a certain height threshold.
+/// </summary>
 using UnityEngine;
 
 public class ObjectDestroyer : MonoBehaviour
 {
     public float heightThreshold = -10f; // The height below which objects will be destroyed
 
+    
     void Update()
     {
-        GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>() ;
-        foreach(GameObject go in allObjects)
+        GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>() ; // Find all game objects
+        foreach(GameObject go in allObjects) // Iterate through them
         {
-            Rigidbody rb = go.GetComponent<Rigidbody>();
-            if (rb != null && go.transform.position.y < heightThreshold)
+            Rigidbody rb = go.GetComponent<Rigidbody>(); // Get the rigidbody
+            if (rb != null && go.transform.position.y < heightThreshold) // If the rigidbody exists and the object is below the threshold
             {
-                Destroy(go);
+                Destroy(go); // Destroy the object
             }
         }
     }

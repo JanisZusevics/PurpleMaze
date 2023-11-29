00094ffff08110f6adb8a26fd7b38aaa42020735
Janis Zusevics <j.zusevics@gmail.com>
2023-11-29 09:43:51 +0000

    commented collectableBehaviour.cs

--- Scripts/Extra/TileManagement/Collectable/collectableBehaviour.cs
+++ Scripts/Extra/TileManagement/Collectable/collectableBehaviour.cs
@@ -21,6 +21,7 @@ public class Collectable : MonoBehaviour
     // OnTriggerEnter is called when the Collider other enters the trigger
     private void OnTriggerEnter(Collider other)
     {
+        // Check if the other object is the player and is active
         if (other.gameObject.CompareTag("Mouse") && other.gameObject.GetComponent<MouseBehaviour>().IsActive)
         {
             Collect();

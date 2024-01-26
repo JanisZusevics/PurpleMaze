735fdf0b4fd3c3a097bbc71007a56303f6f03068
Janis Zusevics <j.zusevics@gmail.com>
2024-01-26 21:45:35 +0000

    hammer good follow jaa

--- Scripts/Hammer/hammeBehaviour.cs
+++ Scripts/Hammer/hammeBehaviour.cs
@@ -24,12 +24,13 @@ public class hammeBehaviour : MonoBehaviour
         // set hammer position high above the crown
         hammer.transform.position = new Vector3(crown.transform.position.x, crown.transform.position.y + 20, crown.transform.position.z);
 
-        crown = GameObject.Find("Crown");
         currentState = State.Awakening;
     }
     // Start is called before the first frame update
     void Start()
     {
+        crown = GameObject.Find("Crown");
+
         crownPosition = crown.transform.position;
         getDesiredPosition();
     }
@@ -57,6 +58,14 @@ public class hammeBehaviour : MonoBehaviour
 
                 break;
             case State.Following:
+                // set desired position to crown position
+                getDesiredPosition();
+                // while hammer is not within 1f of desired position
+                if (Vector3.Distance(transform.position, desiredPosition) > 1f)
+                {
+                    // move hammer towards desired position
+                    transform.position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * 5);
+                }
                 break;
             case State.Striking:
                 break;
@@ -67,10 +76,11 @@ public class hammeBehaviour : MonoBehaviour
 
     private void getDesiredPosition()
     {
+        crownPosition = crown.transform.position;
         // set desired position to crown position
         desiredPosition = crownPosition;
         // set desired position y to 0
-        desiredPosition.y = 10;
+        desiredPosition.y = 30;
     }
 
     /// <summary>

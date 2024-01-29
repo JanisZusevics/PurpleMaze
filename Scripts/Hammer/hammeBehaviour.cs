d770d168c48b3012c1f57f94daa40680e66ed9eb
Janis Zusevics <j.zusevics@gmail.com>
2024-01-29 23:00:11 +0000

    Useless comment

--- Scripts/Hammer/hammeBehaviour.cs
+++ Scripts/Hammer/hammeBehaviour.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+///  Hammer behaviour script using math animations 
+/// </summary>
 public class hammeBehaviour : MonoBehaviour
 {
 
